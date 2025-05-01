20de11c3496f77fe89993bc21ea3db109d13c24d
nairx <nairxprv@gmail.com>
2025-05-01 14:36:25 +0530

    Updated

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 const app = express();
 app.use(cors());
-app.use(express.json())
-const uri = "mongodb://localhost:27017"
-const client = new MongoClient(uri)
-const db = client.db("mernappdb")
+app.use(express.json());
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+const db = client.db("mernappdb");
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.get("/products", async (req, res) => {
-  const products = await db.collection("products").find().toArray()
+  const products = await db.collection("products").find().toArray();
+  res.json(products);
+});
+
+app.post("/products", async (req, res) => {
+  const { name, price } = req.body;
+  const newProduct = {
+    name: name,
+    price: price,
+  };
+  const products = await db.collection("products").insertOne(newProduct);
   res.json(products);
 });
 app.listen(8081, () => {
   console.log("Server started on port 8081");
 });
-
-
